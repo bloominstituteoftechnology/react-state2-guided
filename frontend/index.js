bb196d29bd2478d4a2e613135f7971b8fb86b5df
Ladrillo <gabriel@cabrejas.net>
2022-05-17 18:22:14 +0200

    committing to "lecture"

--- frontend/index.js
+++ frontend/index.js
@@ -9,6 +9,9 @@ import './styles/styles.css'
 import { Provider } from 'react-redux'
 import { legacy_createStore, compose } from 'redux'
 
+// action types
+const INCREMENT = 'INCREMENT'
+
 // dummy combined reducer
 const initialState = { count: 0 }
 function reducer(state = initialState, action) {
