2465dd64c8703d89152190c2ae393040972a2398
Ladrillo <gabriel@cabrejas.net>
2022-05-17 18:26:09 +0200

    committing to "lecture"

--- frontend/index.js
+++ frontend/index.js
@@ -25,7 +25,7 @@ const initialState = { count: 0 }
 function reducer(state = initialState, action) {
   switch(action.type) {
     case INCREMENT: {
-      //
+      return 
     }
     default:
       return state
