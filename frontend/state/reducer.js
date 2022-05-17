3a5b5eea29de65b1c46498f15a816ead34ece2f8
Ladrillo <gabriel@cabrejas.net>
2022-05-17 18:52:33 +0200

    committing to "lecture"

--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -2,7 +2,11 @@ import { combineReducers } from 'redux'
 import * as types from './action-types'
 
 function count(countState = 0, action) {
-  
+  switch(action.type) {
+
+    default:
+      return countState
+  }
 }
 
 export default combineReducers({
