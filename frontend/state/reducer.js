f5015ac94c5d6aef4f308e10d5badeef09678686
Ladrillo <gabriel@cabrejas.net>
2022-05-17 18:53:13 +0200

    committing to "lecture"

--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -3,7 +3,9 @@ import * as types from './action-types'
 
 function count(countState = 0, action) {
   switch(action.type) {
-
+    case types.INCREMENT: {
+      return 
+    }
     default:
       return countState
   }
