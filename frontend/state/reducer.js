2f6c3ea66cfc6e5f112591a0d029c5d64f9c4aa1
Ladrillo <gabriel@cabrejas.net>
2022-05-17 19:55:14 +0200

    committing to "lecture"

--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -21,7 +21,7 @@ function todos(todosState = initialTodos, action) {
   switch (action.type) {
     case types.ADD_TODO: {
       const newTodo = action.payload
-      return newTodo
+      return {}
     }
     default:
       return todosState
