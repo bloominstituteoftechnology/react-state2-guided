536d5481bc072397584298c22845114a7988d399
Ladrillo <gabriel@cabrejas.net>
2022-05-17 18:51:33 +0200

    committing to "lecture"

--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -1,26 +1,13 @@
 import { combineReducers } from 'redux'
 import * as types from './action-types'
 
-function count() {
-
-}
-function count() {
-
-}
-function count() {
-
-}
-function count() {
+const 
+function count(countState = ) {
 
 }
 
 export default combineReducers({
   // here go your reducers combined
   // into one super big reducer
-  count,
-  count,
-  count,
-  count,
-  count,
   count,
 })
