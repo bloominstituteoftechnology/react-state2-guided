de599829ebbd88935e69e2c1a04fee5106a52c70
Ladrillo <gabriel@cabrejas.net>
2022-08-16 16:07:37 +0200

    ready

--- frontend/index.js
+++ frontend/index.js
@@ -5,7 +5,23 @@ import App from './components/App'
 import './styles/reset.css'
 import './styles/styles.css'
 
+// ==================== redux-related scaffolding start ====================
+// import { Provider } from 'react-redux'
+// import { legacy_createStore, compose } from 'redux'
+// import reducer from './state/reducer'
+
+// let store
+// export const resetStore = () => {
+//   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+//   store = legacy_createStore(reducer, composeEnhancers())
+// }
+// resetStore()
+// ===================== redux-related scaffolding end =====================
+
 const container = document.getElementById('root')
 const root = createRoot(container)
 
-root.render(<App />)
+// todo: wrap the App in the Provider
+root.render(
+  <App />
+)
