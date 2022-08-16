7c4dc2efa913590352c2e024f712fea9d0478b36
Ladrillo <gabriel@cabrejas.net>
2022-08-16 18:19:50 +0200

    committing to "lecture"

--- frontend/index.js
+++ frontend/index.js
@@ -6,16 +6,16 @@ import './styles/reset.css'
 import './styles/styles.css'
 
 // ==================== redux-related scaffolding start ====================
-// import { Provider } from 'react-redux'
-// import { legacy_createStore, compose } from 'redux'
-// import reducer from './state/reducer'
+import { Provider } from 'react-redux'
+import { legacy_createStore, compose } from 'redux'
+import reducer from './state/reducer'
 
-// let store
-// export const resetStore = () => {
-//   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-//   store = legacy_createStore(reducer, composeEnhancers())
-// }
-// resetStore()
+let store
+export const resetStore = () => {
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+  store = legacy_createStore(reducer, composeEnhancers())
+}
+resetStore()
 // ===================== redux-related scaffolding end =====================
 
 const container = document.getElementById('root')
