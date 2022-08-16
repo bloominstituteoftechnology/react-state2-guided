c1cfb9303a484fccb16e6dfbc230073793ce25e6
Ladrillo <gabriel@cabrejas.net>
2022-08-16 18:48:54 +0200

    committing to "lecture"

--- frontend/state/action-creators.js
+++ frontend/state/action-creators.js
@@ -2,6 +2,6 @@ import { getId } from '../utils/helpers'
 import * as types from './action-types'
 
 // put your action creators here
-export changeInput({ name, value }) {
-  
+export function changeInput({ name, value }) {
+
 }
