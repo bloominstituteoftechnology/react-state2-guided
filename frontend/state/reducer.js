814b38a61d484d6589407a8b9d2c9cf61d1cdfbc
Ladrillo <gabriel@cabrejas.net>
2022-08-16 18:10:07 +0200

    committing to "lecture"

--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -5,7 +5,9 @@ import * as types from './action-types'
 // put your reducer functions here
 // each reducer takes care of a single
 // slice of state
-
+function form(state, action) {
+  return state
+}
 
 export default combineReducers({
   // here go your reducers combined
